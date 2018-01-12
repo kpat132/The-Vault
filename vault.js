44d2ddad817e03848456f30dd3ab2d05e2593bcd
KrishnaP <kpat132@gmail.com>
2018-01-12 08:58:40 -1000

    changing pr from devleague to mine

--- vault.js
+++ vault.js
@@ -1,24 +1,24 @@
 'use strict';
-module.exports = function() {
+module.exports = function () {
   //define an object
   var obj = {};
 
   //creates a function setValue that sets the objects key equal to its value.
-  function setValue (key, value){
+  function setValue(key, value) {
     obj[key] = value;
   }
-  function getValue(key){
-//if the key is undefined OR if the key's value does not exists then return null
-    if(key === undefined || !obj[key] ){
+  function getValue(key) {
+    //if the key is undefined OR if the key's value does not exists then return null
+    if (key === undefined || !obj[key]) {
       return null;
     }
-//else return the key's corresponding value
-    else{
+    //else return the key's corresponding value
+    else {
       return obj[key];
     }
   }
   //return the functions
-  return{
+  return {
     setValue: setValue,
     getValue: getValue
   }
