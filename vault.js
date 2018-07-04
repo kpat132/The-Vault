fc442a020ed889aab9810bdd459b54897c907c9c
KrishnaP <kpat132@gmail.com>
2018-07-04 13:20:50 -0700

    complted

--- vault.js
+++ vault.js
@@ -1,4 +1,24 @@
 'use strict';
 module.exports = function() {
+
+  let obj = {};
+
+  function setValue(key,value){
+    obj[key]=value;
+
+  }
+  function getValue(key){
+    if(obj[key]){
+      return obj[key]
+    }
+    else{
+      return null;
+    }
+  }
+
+  return{
+    setValue,
+    getValue
+  }
   
 };
